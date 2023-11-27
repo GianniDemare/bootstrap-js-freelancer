6dffcb91ad2a33f9a10c5fda4c6121bf92a64239
xgiannix <gianni.demare@hotmail.it>
2023-11-27 12:55:53 +0100

    Final Commit

--- js/main.js
+++ js/main.js
@@ -2,14 +2,47 @@
 
 let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
+let projectCards = [{
+    name: "Cabin",
+    Image: "cabin",
+},
+{
+    name: "Cake",
+    Image: "cake",
+},
+{
+    name: "Circus",
+    Image: "circus",
+},
+{
+    name: "Game",
+    Image: "game",
+},
+{
+    name: "Safe",
+    Image: "safe",
+},
+{
+    name: "Submarine",
+    Image: "submarine",
+}]
+
+for(let i=0; i <= discountCodes.length; i++) {
+    addCard(projectCards[i])
+
+};
+
+
 function buttonService(event) {
     event.preventDefault();
     
     let hoursWork = document.getElementById("hours").value;
     let typeWork = document.getElementById("typeChoice").value;
-    let userDiscount = document.getElementById("discountCode").value;
+    let userDiscount = document.getElementById("discount").value;
 
-    let price;
+    let price =0;
+    price = parseFloat(price);
+
     if (typeWork == "backend") {
         price = hoursWork * 20.5;
         typeWork = "Back-End Developer"
@@ -20,20 +53,39 @@ function buttonService(event) {
         price = hoursWork * 33.6;
         typeWork = "Project Analyst"
     }
-
+    
     if (discountCodes.includes(userDiscount)) {
         price = price / 100 * 75;
     }
 
     if (userDiscount.length >= 1 && !discountCodes.includes(userDiscount)) {
         document.getElementById("discountMessage").classList.remove("d-none");
-        return false;
     }
 
-    document.getElementById("formDiv").classList.add("d-none");
-    document.getElementById("finalContainer").classList.remove("d-none");
+    
 
-    document.getElementById("priceId").innerHTML = "Price: €" + price.toFixed(2);
-    document.getElementById("hoursId").innerHTML = "Hours requested: " + hoursWork;
-    document.getElementById("workId").innerHTML = "Type of work: " + typeWork;
+    document.getElementById("priceId").innerHTML = "Price: " + price.toFixed(2) + "€";
+    document.getElementById("hoursId").innerHTML = "Hours Requested: " + hoursWork;
+    document.getElementById("workId").innerHTML = "Type of Work: " + typeWork;
 }
+
+function addCard(nameOfArray) {
+    document.getElementById("cardList").innerHTML += `
+    <div class="col-md-6 col-lg-4 mb-5">
+    <div class="card">
+    <img class="card-img-top" src="img/${nameOfArray.Image}.png" alt="cabin">
+    <div class="card-body text-center">
+      <h5>${nameOfArray.name} Website</h5>
+      <a href="#" class="card-link btn btn-info">Preview</a>
+      <a href="#" class="card-link btn btn-outline-info">Visit site</a>
+    </div>
+  </div>
+  </div>
+    `;
+}
+    
+
+    
+
+
+
